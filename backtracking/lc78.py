ad0bf9bc153cb1030a4e3abd73f7deeea19c5580
Rahul Yadav <parryrpy@gmail.com>
2024-05-24 00:58:08 +0530

    More solutions for backtrackng

--- backtracking/lc78.py
+++ backtracking/lc78.py
@@ -37,6 +37,38 @@ class Solution:
             sol.pop()
         backtrack(0)
         return res
+    
+# Solution 3: Using Iterative approach
+# class Solution:
+#     def subsets(self, nums: List[int]) -> List[List[int]]:
+#         subsets = [[]]  # Start with the empty subset
+        
+#         for num in nums:
+#             new_subsets = []
+#             for subset in subsets:
+#                 new_subset = subset + [num]  # Create a new subset by adding the current number
+#                 new_subsets.append(new_subset)
+#             subsets.extend(new_subsets)  # Add the new subsets to the list of all subsets
+        
+#         return subsets
+
+
+# Solution 4: Using Bit Manipulation
+# class Solution:
+#     def subsets(self, nums: List[int]) -> List[List[int]]:
+#         n = len(nums)
+#         all_subsets = []
+        
+#         # Iterate over all possible subsets (2^n)
+#         for mask in range(1 << n):  # 1 << n generates the number of subsets (2^n)
+#             subset = []
+#             for i in range(n):
+#                 # Check if the i-th bit is set in the mask
+#                 if mask & (1 << i):  # 1 << i is used to check the i-th bit in mask
+#                     subset.append(nums[i])
+#             all_subsets.append(subset)
+        
+#         return all_subsets
 
 test = Solution()
 result = test.subsets([1,2,3])
